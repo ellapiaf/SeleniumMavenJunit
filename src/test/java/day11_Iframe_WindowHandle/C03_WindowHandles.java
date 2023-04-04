2984e56092499bc737389384cb89a7aeda8fa988
techproed <burcupiaf@gmail.com>
2023-04-03 23:23:38 -0500

    update april

---
+++ src/test/java/day11_Iframe_WindowHandle/C03_WindowHandles.java
@@ -0,0 +1,27 @@
+package day11_Iframe_WindowHandle;
+
+import org.junit.Test;
+import utilities.TestBase;
+
+public class C03_WindowHandles  extends TestBase {
+
+    @Test
+    public void windowHandles() {
+
+
+//    https://the-internet.herokuapp.com/windows adresine gidin.
+        driver.get("https://the-internet.herokuapp.com/windows");
+//    İlk sayfanın ID'sini(Window Handle değerini) alın.
+//    Sayfadaki textin “Opening a new window” olduğunu doğrulayın.
+//    Sayfa başlığının(title) “The Internet” olduğunu doğrulayın.
+//    "Click Here" butonuna tıklayın.
+//    Açılan pencereye geçin.
+//    Açılan yeni pencerenin sayfa başlığının(title) “New Window” olduğunu doğrulayın.
+//    Bir önceki pencereye dönüp sayfa başlığının “The Internet” olduğunu  doğrulayın.
+//    2. pencereye tekrar geçin.
+//    1. pencereye tekrar dönün.
+
+
+
+    }
+}
